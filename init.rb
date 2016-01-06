1f4041f80c938d5661fa72c84ec0d06e0f6db789
MYchaieb <chaieb.mohamed.yassine@gmail.com>
2016-01-06 11:27:35 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,6 +3,6 @@ Redmine::Plugin.register :signature do
   author 'Chaieb Yassine'
   description 'This is a plugin for Redmine, no need to write every time your signature write it once and it will appear on every comment you write'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/MYchaieb/user_signature_redmine/'
+  author_url ''
 end
