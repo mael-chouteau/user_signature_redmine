68bc49333f7191fe9a11f266ff2a0331850e90f1
Maël Chouteau <101983244+mael-chouteau@users.noreply.github.com>
2024-07-18 16:22:17 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :signature do
   name 'Signature plugin'
   author 'Chaieb Yassine'
   description 'This is a plugin for Redmine, no need to write every time your signature write it once and it will appear on every comment you write'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/MYchaieb/user_signature_redmine/'
   author_url ''
 end
